0a3a5a882bef4f6ba645badca9d9dd356df95f1e
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 12:04:41 -0500

    add soda.js and sodalist.js, adjust app.js code to just have header and soda list

---
+++ src/components/Soda.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Soda() {
+  return(
+    <h1>
+    <h1>Soda placeholders</h1>
+    </h1>
+  );
+}
+
+export default Soda;
---
+++ src/components/SodaList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Soda from './Soda';
+
+function SodaList() {
+  return (
+    <Soda />
+  );
+}
+
+export default SodaList;
