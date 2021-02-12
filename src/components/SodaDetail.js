22d957da02a763bd41d96f136f2a6668a06dbb9a
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 17:36:45 -0500

    add delete button to soda detail

--- src/components/SodaDetail.js
+++ src/components/SodaDetail.js
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function SodaDetail(props) {
-  const { soda } = props;
+  const { soda, onClickingDelete } = props;
 
   return (
     <>
     <h1>Soda Detail</h1>
     <h3>{soda.name} - {soda.brand}</h3>
     <h3>{soda.price} - {soda.flavor}</h3> 
+    <button onClick={()=> onClickingDelete(soda.id) }>Close out of Soda Detail</button>
     </>
   );
 }
 
 SodaDetail.propTypes = {
-  soda: PropTypes.object
+  soda: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default SodaDetail;
