659bee2eecaa2ac7345adb7c7700ca08a9d4deb9
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 17:57:42 -0500

    add edit button to soda detail page

--- src/components/SodaDetail.js
+++ src/components/SodaDetail.js
@@ -9,6 +9,7 @@ function SodaDetail(props) {
     <h1>Soda Detail</h1>
     <h3>{soda.name} - {soda.brand}</h3>
     <h3>{soda.price} - {soda.flavor}</h3> 
+    <button onClick={ props.onClickingEdit }>Edit Soda</button>
     <button onClick={()=> onClickingDelete(soda.id) }>Close out of Soda Detail</button>
     </>
   );
