93a086021f82fe416c8dabb98df8280fdf154b71
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 13:38:18 -0500

    add passing props to sodalist and add prop types

--- src/components/SodaList.js
+++ src/components/SodaList.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Soda from './Soda';
+import PropTypes from 'prop-types';
 
-function SodaList() {
+function SodaList(props) {
   return (
-    <Soda />
+    <>
+    {props.sodaList.map((soda, index) =>
+      <Soda
+      name={soda.name}
+      brand={soda.brand}
+      price={soda.price}
+      flavor={soda.flavor}
+      key={index} />
+      )}
+    </>
   );
 }
+
+SodaList.propTypes = {
+  sodaList: PropTypes.array
+};
 
 export default SodaList;
