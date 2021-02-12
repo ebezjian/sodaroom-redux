370ef7adf0b7dd00103c0cad4fb42d681d74e22b
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 18:08:00 -0500

    add editsodaform and function

---
+++ src/components/EditSodaForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+function EditSodaForm (props) {
+  return (
+    <>
+    <ReusableForm
+      buttonText="Edit Soda" />
+    </>
+  );
+}
+
+export default EditSodaForm;
