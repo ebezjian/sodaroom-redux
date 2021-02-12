aef4eac207cb03f3458cf6e0c5faba0602967e42
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 18:20:39 -0500

    add event handler and function to get form values to edit form

--- src/components/EditSodaForm.js
+++ src/components/EditSodaForm.js
@@ -3,9 +3,22 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditSodaForm (props) {
+  const { soda } = props;
+
+  function handleEditSodaFormSubmission(event) {
+    event.preventDefault();
+    props.onEditSoda({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      flavor: event.target.flavor.value,
+      id: soda.id,
+    });
+  }
   return (
     <>
     <ReusableForm
+    formSubmissionHandler={handleEditSodaFormSubmission}
       buttonText="Edit Soda" />
     </>
   );
