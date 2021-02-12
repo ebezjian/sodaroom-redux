50c697c31a2231108a34e312fc40c6317970820f
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 16:26:20 -0500

    add props to SodaDetail

--- src/components/SodaDetail.js
+++ src/components/SodaDetail.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function SodaDetail(props) {
+  const { soda } = props;
+
   return (
     <>
     <h1>Soda Detail placeholder</h1>
+    <h3>{soda.name} - {soda.brand}</h3>
+    <h3>{soda.price} - {soda.flavor}</h3> 
     </>
   );
 }
+
+SodaDetail.propTypes = {
+  soda: PropTypes.object
+};
 
 export default SodaDetail;
