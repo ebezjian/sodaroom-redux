ae52052a01ea6c15fbbc948f459971d13f2a719a
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 12:08:34 -0500

    add proptypes to soda.js

--- src/components/Soda.js
+++ src/components/Soda.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Soda() {
+function Soda(props) {
   return(
     <h1>
     <h1>Soda placeholders</h1>
     </h1>
   );
+}
+
+Soda.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  flavor: PropTypes.string,
 }
 
 export default Soda;
