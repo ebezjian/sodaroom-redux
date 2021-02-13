9b6b475e768da4f1f8f6a7d2308773ccc3724ffe
Ellie Lambert <ebezjian@gmail.com>
2021-02-12 19:49:53 -0500

    starting to add quantity props and recursive function to restock

--- src/components/SodaDetail.js
+++ src/components/SodaDetail.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as $ from 'jquery';
 
 function SodaDetail(props) {
   const { soda, onClickingDelete } = props;
-
+  function checkquantity (num) {
+    if(num > 0 $$ num <124){
+      return(
+        <>
+          <input type="number" name="restock" id="restock"/>
+          <buton onClick={()=>props.onRestocking(soda.id, parseInt("#restock").val()))}>Restock</button>
+          <button onClick={()=>props.onBuying(soda.id)}>Buy Soda</button>
+        </>
+      )
+    }else if(num > 0) {
+      return <button onClick={()=>props.onBuying(soda.id)}>Buy Soda</button>
+    } else if (num < 124) {
+      return(
+        <>
+          <input type="number" name="restock" id="restock"/>
+          <button onClick={()=>props.onRestocking(soda.id, parseInt($("#restock").val()))}>Restock</button>
+        </>
+      );
+    }
+  }
+  const stockElements = checkquantity(soda.quantity);
   return (
     <>
     <h1>Soda Detail</h1>
-    <h3>{soda.name} - {soda.brand}</h3>
-    <h3>{soda.price} - {soda.flavor}</h3> 
+    <h3>{soda.name} by {soda.brand}</h3>
+    <h3>${soda.price} Per Pint </h3>
+    <h3>{soda.quantity}</h3>
+    <h3> {soda.flavor}</h3> 
+    {stockElements}
     <button onClick={ props.onClickingEdit }>Edit Soda</button>
     <button onClick={()=> onClickingDelete(soda.id) }>Close out of Soda Detail</button>
     </>
