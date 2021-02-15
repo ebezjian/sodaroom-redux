73c31201005e46701ed0aabba0b65694c30bf4f2
Ellie Lambert <ebezjian@gmail.com>
2021-02-15 01:50:49 -0500

    add more styling

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,36 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button} from 'react-bootstrap';
+import {Button, Col, Form, Container} from 'react-bootstrap';
 
 function ReusableForm(props) {
   return(
     <>
-      <form onSubmit={props.formSubmissionHandler}>
-      <div class="form-floating mb-3">
-        <input
-            type="text"
-            class="form-control"
-            id="floatingInput"
-            name="name"                     
-            placeholder="Name of Soda"/>  
-        
-      </div>
-      
-          <input
-            type="text"
-            name="brand"
-            placeholder="Brand of Soda" />
-          <input
-            type="number"
-            name="price"
-            placeholder="Price of Soda" />
-          <input
-            type="text"
-            name="flavor"
-            placeholder="Flavor of Soda" />
-          <Button   variant="outline-secondary" type="submit">{props.buttonText}</Button>
-      
-      </form>
+    <Container>
+      <Form onSubmit={props.formSubmissionHandler} className="test" >
+        <Form.Row className="align-items-center" >
+          <Col xs="auto" className="test">
+        <Form.Group controlId="formGroupName">
+          <Form.Label>Name of Soda </Form.Label>
+          <Form.Control                       
+              type="text"
+              name="name"                     
+              placeholder="Name of Soda"/>
+          </Form.Group>    
+        </Col> 
+        <Col xs="auto">
+        <Form.Group controlId="formGroupBrand">
+          <Form.Label>Name of Brand </Form.Label>         
+            <Form.Control
+              type="text"
+              name="brand"
+              placeholder="Brand of Soda" />
+        </Form.Group>
+        </Col>
+        <Col xs="auto">
+        <Form.Label>Price of Soda</Form.Label>
+        <Form.Group>
+            <Form.Control
+              type="number"
+              name="price"
+              placeholder="Price of Soda" />
+        </Form.Group>
+        </Col>
+        <Col xs="auto" >
+        <Form.Group>
+          <Form.Label>Flavor of Soda</Form.Label>
+            <Form.Control
+              type="text"
+              name="flavor"
+              placeholder="Flavor of Soda" />
+        </Form.Group>
+        </Col >
+            <Button   variant="outline-secondary" type="submit">{props.buttonText}</Button>
+        </Form.Row>
+      </Form>
+      </Container>
     </>
   );
 }
