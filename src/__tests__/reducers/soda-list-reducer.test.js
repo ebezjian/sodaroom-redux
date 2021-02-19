e8edc38b21065da6b959464fd69097bc6a8933c3
Ellie Lambert <ebezjian@gmail.com>
2021-02-19 13:26:46 -0500

    passing test

--- src/__tests__/reducers/soda-list-reducer.test.js
+++ src/__tests__/reducers/soda-list-reducer.test.js
@@ -1,7 +1,7 @@
 import sodaListReducer from '../../reducers/soda-list-reducer';
 
-describe('sodaListReducer'), () => {
+describe('sodaListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(sodaListReducer({}, {type: null})).toEqual({});
+    expect(sodaListReducer({}, { type: null })).toEqual({});
   });
-};
+});
