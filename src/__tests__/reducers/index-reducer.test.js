a0169c7d67956a484c29f85647e46813a8f848a0
Ellie Lambert <ebezjian@gmail.com>
2021-02-19 16:41:33 -0500

    adjust index test to return all passing

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -3,9 +3,9 @@ import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import sodaListReducer from '../../reducers/soda-list-reducer';
 
-let store = createStore;
 
 describe("rootReducer", () => {
+  let store = createStore(rootReducer);
   
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
